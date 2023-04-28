f135fbd5f6c8b869f79ed5cb08f70aea166b596f
Jasmine Grace <Ja64970@gmail.com>
2023-04-27 22:13:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+for ( let i = 1; i <=10; i++) {
+console.log(i)
+}
+
+// for (let i = 1; i <=10; i++) {
+// console.log(i)
+// }
+    
+let count = 10;
+
+console.log ("while")
+let i = count;
+while (i <= 10) {
+console.log(i);
+}
+
+// for (let j = 10; j >= 1; j--) {
+// console.log(j);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// let j = 10; 
+// do {
+//     console.log(j);
+//     j--;
+// } while (j >= 1);
